c319b01ab1cfb3bdcf9f22344b46491baff94108
ExternalAddress4401 <external_address@hotmail.com>
2024-08-24 20:15:30 -0700

    Modified directories

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const axios = require("axios");
-const cheerio = require("cheerio");
 const fs = require("fs");
 
 const url =
   "https://apkpure.com/beatstar-touch-your-music/com.spaceapegames.beatstar/downloading";
 
 async function start() {
-  const apk = await axios.get(url, { responseType: "stream" });
-  apk.data.pipe(fs.createWriteStream("./beatstar.apk"));
+  const fetchUrl = url
+    .replace("{base64}", core.getInput("data-url"))
+    .replace("{version}", core.getInput("version").replaceAll(".", "-"));
+
+  const apkResponse = await axios.get(fetchUrl, { responseType: "stream" });
+  apkResponse.data.pipe(fs.createWriteStream("./apk/beatstar.apk"));
 }
 
 start();
